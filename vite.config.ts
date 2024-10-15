da92e490f5a15c3bb23a5284ca048e906965addc
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-10-15 20:30:10 +0300

    add start files

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
+
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: '[name].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    css: {
+      postcss: {
+        plugins: [
+          SortCss({
+            sort: 'mobile-first',
+          }),
+        ],
+      },
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html', './src/**/*.scss', './src/**/*.ts']),
+    ],
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+  };
+});
