86e75ef17930595025e7027dee19607f2ea664ce
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-10-25 12:38:48 +0300

    Update api.ts

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -1,4 +1,4 @@
-import {IFilter} from './interface'
+import {IFilter, IExerciseFilter} from './interface'
 
 const BASE_URL = 'https://energyflow.b.goit.study/api';
 
@@ -14,8 +14,8 @@ async function fetchQuote() {
     return response
 }
 
-async function fetchExercises({filterName, currentPage}:IFilter) {
-    const response = fetch(`${BASE_URL}/filters?filter=${filterName}&page=${currentPage}&limit=${12}`)
+async function fetchFilterExercises({filterName, currentPage}:IFilter) {
+    const response = fetch(`${BASE_URL}/filters?filter=${filterName}&page=${currentPage}&limit=12`)
         .then(resp => {
         if (!resp.ok) {
             throw new Error(`${resp.statusText}`);
@@ -26,4 +26,23 @@ async function fetchExercises({filterName, currentPage}:IFilter) {
     return response
 }
 
-export {fetchQuote, fetchExercises}
+async function fetchSubtypeExercises(filtername: string, exercise: string) {
+    let newFilterName: string = ''
+    if (filtername === 'body parts' && filtername.endsWith('s')) {
+        const joinedFilterName = filtername.split(' ').join('')
+        newFilterName = joinedFilterName.slice(0,joinedFilterName.length-1)
+    } else {
+        newFilterName = filtername
+    }
+    const response = fetch(`${BASE_URL}/exercises?${newFilterName}=${exercise}&page=1&limit=12`)
+        .then(resp => {
+        if (!resp.ok) {
+            throw new Error(`${resp.statusText}`);
+        }
+            return resp.json()    
+        })
+        .catch(err => console.log(err))
+    return response
+}
+
+export {fetchQuote, fetchFilterExercises, fetchSubtypeExercises}
