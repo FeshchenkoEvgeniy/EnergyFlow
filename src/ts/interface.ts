124070784cc1da6ff0ab383ffd9e8a52f11e11d8
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-10-25 12:38:54 +0300

    Update interface.ts

--- src/ts/interface.ts
+++ src/ts/interface.ts
@@ -3,10 +3,25 @@ interface IFilter{
     currentPage: string
 }
 
+interface IExerciseFilter{
+    filtername: string,
+    exercise: string
+}
+
 interface IResultExercises {
     name: string,
     filter: string,
     imgUrl: string,
 }
 
-export type {IFilter, IResultExercises}
+interface ISubtypeExercise{
+    bodyPart: string,
+    burnedCalories: number,
+    name: string,
+    rating: number,
+    target: string,
+    time: number,
+    _id: string
+}
+
+export type {IFilter, IResultExercises, IExerciseFilter, ISubtypeExercise}
