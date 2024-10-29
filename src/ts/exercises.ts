5ea96c8983882d251ee883db97c2058b83a5fb42
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-10-29 20:29:38 +0200

    Update exercises.ts

--- src/ts/exercises.ts
+++ src/ts/exercises.ts
@@ -1,26 +1,19 @@
-import { fetchFilterExercises } from "./api";
-import { createMarkupForExercises, createMarkupForExercisesPagination } from "./createMarkup";
 import { IFilter,} from './interface'
+import { renderMarkup } from "./pagination";
 
 const filterBtn = document.querySelector('.js-filterBtn') as HTMLElement
-const exercisesList = document.querySelector('.js-exercises__list') as HTMLElement;
-const subtypeExercisesList = document.querySelector('.js-subtype-exercises__list') as HTMLElement
-const paginationList = document.querySelector('.js-exercises__pagaination') as HTMLElement
 const buttons = document.querySelectorAll('.filter__btn') as NodeListOf<HTMLButtonElement>;
 
-const initialFilter: IFilter = {
+export const initialFilter: IFilter = {
     filterName: 'Muscles',
-    currentPage: '1'
+    exercises: 'calves',
+    currentPage: '1',
 }
 
-let paginationBtns: NodeListOf<HTMLButtonElement>;
-
+const listType = 'exercises'
 
 function onClick(evt: Event) {
     const target = evt.target as HTMLElement;
-
-    exercisesList.style.display = 'flex'
-    subtypeExercisesList.style.display = 'none'
 
     if (!target.classList.contains('filter__btn')) {
         return
@@ -33,46 +26,11 @@ function onClick(evt: Event) {
     buttons.forEach(btn => btn.classList.remove('filter__btn--active'))
     target.classList.add('filter__btn--active')
     
-    renderMarkup()
+    renderMarkup(listType, initialFilter.filterName, initialFilter.exercises)
 }
 
-renderMarkup()
+renderMarkup(listType, initialFilter.filterName, initialFilter.exercises)
 
-function renderMarkup() {
-    fetchFilterExercises(initialFilter)
-    .then(({totalPages, results}) => {
-        exercisesList.innerHTML = createMarkupForExercises(results)
 
-        if (totalPages <= 1) {
-            paginationList.innerHTML = ''
-            return
-        } else {
-            paginationList.innerHTML = createMarkupForExercisesPagination(totalPages);
-
-            paginationBtns = document.querySelectorAll('.pagination__btn')
-            paginationBtns[Number(initialFilter.currentPage)-1].classList.add('pagination__btn--active')
-        }
-    })
-    .catch(err => console.log(err))    
-}
-
-function pagination(evt: Event) {
-    const target = evt.target as HTMLElement;
-
-    const paginationItem = target.closest('.pagination__item') as HTMLElement;
-    
-    if (!paginationItem || target.classList.contains('pagination__list')) {
-        return
-    }
-
-    const page = paginationItem.dataset.page
-
-    if (initialFilter.currentPage !== page?.toString()) {
-        initialFilter.currentPage = page?.toString() || '1';
-        renderMarkup()
-        paginationBtns.forEach(btn => btn.classList.remove('pagination__btn--active'))
-    }
-}
 
 filterBtn.addEventListener('click', onClick)
-paginationList.addEventListener('click', pagination)
