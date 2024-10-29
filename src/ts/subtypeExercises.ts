82fe0adfd073309d619b3d0ea090dc2464f2fc14
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-10-29 21:03:05 +0200

    Update subtypeExercises.ts

--- src/ts/subtypeExercises.ts
+++ src/ts/subtypeExercises.ts
@@ -1,6 +1,7 @@
 import { renderMarkup } from "./pagination";
 
 const exercisesList = document.querySelector('.js-exercises__list') as HTMLElement;
+const exerciseTitle = document.querySelector('.js-exercises__title') as HTMLElement;
 
 function onClick(evt: Event) {
     const target = evt.target as HTMLElement;
@@ -12,6 +13,10 @@ function onClick(evt: Event) {
     const exercisesItem = target.closest('.exercises__item') as HTMLElement
     const { filtername = 'muscles', exercise='calves'} = exercisesItem.dataset
     
+    console.log('filtername:', filtername, 'exercise:', exercise)
+
+    exerciseTitle.insertAdjacentHTML('beforeend', ` / <span class='exercises__title--subtype'>${exercise.charAt(0).toUpperCase() + exercise.slice(1)}</span>`)
+
     renderMarkup(listType, filtername, exercise)
 }
 
