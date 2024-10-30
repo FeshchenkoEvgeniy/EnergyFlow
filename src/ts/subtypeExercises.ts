2a876c008f6f8ae6de92f056d853186c97aa9a72
Evgeniy <feshchenko.evgeniy03@gmail.com>
2024-10-30 20:54:55 +0200

    Update subtypeExercises.ts

--- src/ts/subtypeExercises.ts
+++ src/ts/subtypeExercises.ts
@@ -2,22 +2,37 @@ import { renderMarkup } from "./pagination";
 
 const exercisesList = document.querySelector('.js-exercises__list') as HTMLElement;
 const exerciseTitle = document.querySelector('.js-exercises__title') as HTMLElement;
+const searchContainer = document.querySelector('.search-container') as HTMLElement
+const searchBtn = document.querySelector('.js-filter__search-btn') as HTMLElement
+
+let filterName:string;
+const listType = 'subtypeExercises'
 
 function onClick(evt: Event) {
     const target = evt.target as HTMLElement;
-    const listType = 'subtypeExercises'
 
     if (target.classList.contains('exercises__list')) {
         return
     }
     const exercisesItem = target.closest('.exercises__item') as HTMLElement
     const { filtername = 'muscles', exercise='calves'} = exercisesItem.dataset
-    
-    console.log('filtername:', filtername, 'exercise:', exercise)
+
+    filterName = filtername
 
     exerciseTitle.insertAdjacentHTML('beforeend', ` / <span class='exercises__title--subtype'>${exercise.charAt(0).toUpperCase() + exercise.slice(1)}</span>`)
+
+    searchContainer.style.display = 'block'
 
     renderMarkup(listType, filtername, exercise)
 }
 
+function handleSearchClick() {
+    const searchInput = document.querySelector('.js-filter__search') as HTMLInputElement
+    const query = searchInput.value.trim();
+    if (query.length) {
+        renderMarkup(listType, filterName, query)
+    }
+}
+
+searchBtn.addEventListener('click', handleSearchClick)
 exercisesList.addEventListener('click', onClick)
